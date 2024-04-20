918a3dd5038663015e4a382e9a6f8985c9d67acf
gsq7332 <gsq7332@g.rit.edu>
2024-04-20 14:10:02 -0400

    laying the groundwork for the game section of the ui

--- moonrune-ui/src/app/game/multiple-choice/multiple-choice.component.ts
+++ moonrune-ui/src/app/game/multiple-choice/multiple-choice.component.ts
@@ -1,19 +1,37 @@
-import { Component, Output } from '@angular/core';
+import { Component, Output, Input } from '@angular/core';
 import { GameService } from '../game.service';
 import { EventEmitter } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-multiple-choice',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './multiple-choice.component.html',
   styleUrl: './multiple-choice.component.css'
 })
 export class MultipleChoiceComponent {
-
+  @Input() collection ?: string;
   @Output() endGameEvent = new EventEmitter();
+  hasEnded: boolean = false;
 
   constructor(private gameService: GameService) {}
+
+  ngOnInit() {
+    
+    this.generateQuestion();
+  }
+
+  generateQuestion() {
+    // needed behavior:
+    // get requested properties from settings (part of settings thing)
+    // get list of allowed terms (i.e. the collection)
+  }
+
+  answerQuestion() {
+    // call the service thing
+    // have the api handle behavior
+  }
 
   endGame(): void {
     this.endGameEvent.emit();
