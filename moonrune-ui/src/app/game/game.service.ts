8c1e6138a885a82cb45dab78a96cb5e789369092
gsq7332 <gsq7332@g.rit.edu>
2024-04-21 19:18:16 -0400

    added untested connections between frontend and backend

--- moonrune-ui/src/app/game/game.service.ts
+++ moonrune-ui/src/app/game/game.service.ts
@@ -1,13 +1,17 @@
-import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { ApplicationConfig, Injectable } from '@angular/core';
+import { Observable, catchError, of } from 'rxjs';
+import { Term } from '../terms/term';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
   hasStarted = false;
+  url = "http://localhost:8080/game"
 
-  constructor() { }
+
+  constructor(private http: HttpClient) { }
 
   checkStarted(): Observable<boolean> {
     return of(this.hasStarted);
@@ -18,8 +22,30 @@ export class GameService {
     return of(this.hasStarted);
   }
 
+  generateQuestion(id: number): Observable<Term[]> {
+    let currUrl = this.url + "/generate";
+    return this.http.get<Term[]>(currUrl).pipe(catchError(this.handleError<Term[]>('generateQuestion', [])));
+  }
+
+  getProgress(id: number): Observable<number[]> {
+    return this.http.get<number[]>(this.url)
+    .pipe(catchError(this.handleError<number[]>('getProgress', [])));
+  }
+
+  checkQuestion(id: number, check: string): Observable<boolean> {
+    let currUrl = this.url + "/check";
+    return this.http.get<boolean>(currUrl).pipe(catchError(this.handleError<boolean>('checkQuestion')));
+  }
+
   endGame(): Observable<boolean> {
     this.hasStarted = false;
     return of(this.hasStarted);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      return of(result as T);
+    }
   }
 }
