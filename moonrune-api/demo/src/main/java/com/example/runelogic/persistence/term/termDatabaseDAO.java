e44d72399a09ae34ef614c0fb24e7cfa997cca90
gsq7332 <gsq7332@g.rit.edu>
2024-05-05 23:49:29 -0400

    added import statements to the database file

--- moonrune-api/demo/src/main/java/com/example/runelogic/persistence/term/termDatabaseDAO.java
+++ moonrune-api/demo/src/main/java/com/example/runelogic/persistence/term/termDatabaseDAO.java
@@ -3,13 +3,40 @@ package com.example.runelogic.persistence.term;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 
 import org.springframework.beans.factory.annotation.Value;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class termDatabaseDAO extends termDAO {
 
+    private final String userPath = "src/input/user-pass.txt";
+    private String username;
+    private String password;
+    private Connection connection;
+    private String databasePath;
+
     public termDatabaseDAO(@Value("${terms.database}") String database, ObjectMapper objectMapper) throws IOException {
         super();
+        databasePath = database;
+        getUsernamePassword();
+        load();
+    }
+
+    private void getUsernamePassword() {
+        try (
+            FileInputStream fis = new FileInputStream(userPath);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader reader = new BufferedReader(isr);
+        ) {
+            username = reader.readLine();
+            password = reader.readLine();
+        } catch (Exception exception) {
+
+        }
     }
 
     public void save() {
@@ -17,6 +44,10 @@ public class termDatabaseDAO extends termDAO {
     }
 
     public void load() {
-
+        try {
+            connection = DriverManager.getConnection(databasePath, username, password);
+        } catch (Exception exception) {
+            
+        }
     }
 }
