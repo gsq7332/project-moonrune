15dce80220c069727317080bf7f16aa4fa0a7c55
gsq7332 <gsq7332@g.rit.edu>
2024-05-16 21:25:46 -0400

    added base logic for terms-edit

--- moonrune-ui/src/app/dictionary/terms-edit/terms-edit.component.ts
+++ moonrune-ui/src/app/dictionary/terms-edit/terms-edit.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Term } from '../../terms/term';
+import { NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { find } from 'rxjs';
 
 @Component({
   selector: 'app-terms-edit',
   standalone: true,
-  imports: [],
+  imports: [NgFor, FormsModule],
   templateUrl: './terms-edit.component.html',
   styleUrl: './terms-edit.component.css'
 })
 export class TermsEditComponent {
+  originalTerms ?: Term[]
+  currentTerms ?: Term[]
+  @Input() id ?: number
 
+  addTerm() {
+    let newTerm: Term = {term: "", meanings: []}
+    this.currentTerms?.push(newTerm)
+  }
+
+  removeTerm(idx: number) {
+    if (!this.currentTerms) return;
+    this.currentTerms.splice(idx, idx)    
+  }
+
+  removeMeaning(idx: number) {
+    if (!this.currentTerms) return;
+    this.currentTerms?.splice(idx, idx)
+  }
+
+  saveChanges() {
+    for (let term in this.originalTerms) {
+      
+    }
+  }
 }
